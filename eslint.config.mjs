219818a8c3da47c74a932c9c9b8f3fe594671272
~Leviiii~ <69586326+Jhnlevi@users.noreply.github.com>
2025-10-19 18:36:41 +0800

    chore (setup): initialize project structure, tooling, and CI (#1)
    * chore (setup): initialize .gitignore and README.md
    
    * chore (setup): initialize package,json and Playwright
    
    * chore (setup): initialize tsconfig.json
    
    * chore (setup): initialize eslint and prettier
    
    * chore (setup): add custom scripts in package.json
    
    * chore (setup): add additional rule recommended by Playwright
    
    * ci: add workflow for code linting and formatting
    
    * refactor (ci): rename playwright.yml to execute-tests.yml, and update workflow run command
    
    * ci: implement code-checker jobs to run linter and prettier
    
    * feat: add script to execute auto-fix linting and prettier formatting
    
    * chore: run check:fix to fix linting and formatting issues

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default defineConfig([
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  // {
+  //   parserOptions:{
+  //     project: "./tsconfig.json"
+  //   },
+  //   rules: {
+  //       "@typescript-eslint/no-floating-promises": "error"
+  //   }
+  // },
+  eslintConfigPrettier,
+]);
