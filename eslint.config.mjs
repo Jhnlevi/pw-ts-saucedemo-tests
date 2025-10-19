ccc97c10c8e8294de53ace0567fd717a4b826d5f
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-19 18:19:59 +0800

    chore: run check:fix to fix linting and formatting issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,18 @@
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  {
-    rules: {
-        "@typescript-eslint/no-floating-promises": "error"
-    }
-  },
-  eslintConfigPrettier
+  // {
+  //   parserOptions:{
+  //     project: "./tsconfig.json"
+  //   },
+  //   rules: {
+  //       "@typescript-eslint/no-floating-promises": "error"
+  //   }
+  // },
+  eslintConfigPrettier,
 ]);
