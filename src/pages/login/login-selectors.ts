afc82f4b9a9b22614bbd00f91a783c60f1be16d0
~Leviiii~ <69586326+Jhnlevi@users.noreply.github.com>
2025-10-20 09:14:36 +0800

    feat (login-feature): implement tests for login page (#2)
    * ci: add workflow_dispatch on workflow run
    
    * ci (refactor): update on trigger setup of Code Checker workflow
    
    * feat(pages): add LoginPage class in src/pages/login
    
    * feat: add class for login-selectors
    
    * feat (login-selectors): add constant fields for login page
    
    * feat (login-page): implement locators and actions for login page
    
    * chore(playwright.config): set BaseUrl, reporter, and screenshot options
    
    * feat: add 'UI' folder and class for login-tests
    
    * chore (playwright.config): update folder path for both 'outputDir' and 'reporter' to fix pathing issues
    
    * feat: add 'data' folder and class for login test data
    
    * feat (login-data): implement test data for login tests
    
    * refactor (login-page): modify login error message locator
    
    * feat(login-tests): implement login page test methods
    
    * refactor(playwright.config): add timestamp for report and update pathing of artifacts
    
    * refactor: remove example.spec.ts from project
    
    * chore: run check:fix to fix linting and formatting issues

---
+++ src/pages/login/login-selectors.ts
@@ -0,0 +1,6 @@
+export const LOGIN_SELECTORS = {
+  LOGIN_USERNAME: "login-username",
+  LOGIN_PASSWORD: "login-password",
+  LOGIN_BUTTON: "login-button",
+  LOGIN_ERROR_MESSAGE: "login-error-message",
+} as const;
---
+++ tests/UI/login-tests.spec.ts
@@ -0,0 +1,52 @@
+import test, { expect, Locator } from "@playwright/test";
+import { LoginPage } from "../../src/pages/login/login-page";
+import {
+  LoginTestCase,
+  negLoginCases,
+  posLoginCases,
+} from "../../src/data/login-data";
+
+// Combine all test data from login-data.ts
+const testData: LoginTestCase[] = [...posLoginCases, ...negLoginCases];
+
+test.describe("Login Tests", () => {
+  // Valid test cases (positive)
+  testData.forEach(({ description, username, password, type }) => {
+    if (type === "positive") {
+      test(`Login_${description}_ShouldSucceed`, async ({ page }) => {
+        const _login: LoginPage = new LoginPage(page);
+
+        await page.goto("/");
+        await _login.enterText("LOGIN_USERNAME", username);
+        await _login.enterText("LOGIN_PASSWORD", password);
+        await _login.click("LOGIN_BUTTON");
+
+        const inventoryList: Locator = page.locator(".inventory_list");
+
+        // Expects URL to contain the word 'inventory'.
+        await expect(page).toHaveURL(/inventory/);
+        await expect(inventoryList).toBeVisible();
+      });
+    }
+  });
+
+  // Valid test cases (negative)
+  testData.forEach(({ description, username, password, type, error }) => {
+    if (type === "negative") {
+      test(`Login_${description}_ShouldFail`, async ({ page }) => {
+        const _login: LoginPage = new LoginPage(page);
+
+        await page.goto("/");
+        await _login.enterText("LOGIN_USERNAME", username);
+        await _login.enterText("LOGIN_PASSWORD", password);
+        await _login.click("LOGIN_BUTTON");
+
+        const errElement = await _login.isVisible("LOGIN_ERROR_MESSAGE");
+        const errMessage = error?.message ?? "No error";
+
+        await expect(errElement).toBeVisible({ timeout: 5000 });
+        await expect(errElement).toContainText(errMessage);
+      });
+    }
+  });
+});
