0a9f1301ba0359237eef75d9e2a6fd42801bba5e
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-22 15:42:17 +0800

    refactor (inventory-page): add references for menu and header components

--- src/pages/inventory/inventory-page.ts
+++ src/pages/inventory/inventory-page.ts
@@ -1,9 +1,13 @@
 import { Locator, Page } from "@playwright/test";
 import { INVENTORY_SELECTORS } from "./inventory-selectors";
+import { MenuComponent } from "@pages/components/menu-component";
+import { HeaderComponent } from "@pages/components/header-component";
 
 export class InventoryPage {
   private _page: Page;
   private _locators: Record<keyof typeof INVENTORY_SELECTORS, Locator>;
+  public menu: MenuComponent;
+  public header: HeaderComponent;
 
   constructor(page: Page) {
     this._page = page;
@@ -14,7 +18,7 @@ export class InventoryPage {
       INVENTORY_ITEM_DESC: this._page.locator("div.inventory_item_desc"),
       INVENTORY_ITEM_PRICE: this._page.locator("div.inventory_item_price"),
       INVENTORY_ITEM_SORTER: this._page.locator(
-        "select.product_sort_container",
+        "select.product_sort_container"
       ),
       INVENTORY_ADD_TO_CART: this._page.getByRole("button", {
         name: "Add to cart",
@@ -23,11 +27,13 @@ export class InventoryPage {
         name: "Remove",
       }),
     };
+    this.menu = new MenuComponent(page);
+    this.header = new HeaderComponent(page);
   }
 
   async clickItemByName(
     field: keyof typeof INVENTORY_SELECTORS,
-    itemName: string,
+    itemName: string
   ): Promise<void> {
     const item: Locator = await this._locators[field].filter({
       hasText: itemName,
@@ -37,7 +43,7 @@ export class InventoryPage {
 
   async clickAddItemToCart(
     field: keyof typeof INVENTORY_SELECTORS,
-    itemName: string,
+    itemName: string
   ): Promise<void> {
     const item: Locator = await this._locators[field].filter({
       hasText: itemName,
@@ -47,7 +53,7 @@ export class InventoryPage {
 
   async clickRemoveItemFromCart(
     field: keyof typeof INVENTORY_SELECTORS,
-    itemName: string,
+    itemName: string
   ): Promise<void> {
     const item: Locator = await this._locators[field].filter({
       hasText: itemName,
@@ -57,14 +63,14 @@ export class InventoryPage {
 
   async selectItemByValue(
     field: keyof typeof INVENTORY_SELECTORS,
-    value: string,
+    value: string
   ): Promise<Array<string>> {
     return await this._locators[field].selectOption({ value: value });
   }
 
   async getText(
     field: keyof typeof INVENTORY_SELECTORS,
-    itemName: string,
+    itemName: string
   ): Promise<string> {
     return await this._locators[field]
       .filter({ hasText: itemName })
