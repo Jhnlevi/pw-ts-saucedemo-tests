b4e7cc830cc9ae7c9b2abc771488a172e1203128
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-22 17:20:35 +0800

    refactor (auth-fixture): remove interface and add page.goto and page.waitForURL methods

--- src/fixtures/auth-fixture.ts
+++ src/fixtures/auth-fixture.ts
@@ -1,15 +1,15 @@
 import { performLogin } from "@helpers/login-helper";
 import { LoginPage } from "@pages/login/login-page";
-import { test as base, Page } from "@playwright/test";
+import { test as base, expect, Page } from "@playwright/test";
 
-interface Fixtures {
-  loggedIn: Page;
-}
-
-export const test = base.extend<Fixtures>({
+export const test = base.extend<{ loggedIn: Page }>({
   loggedIn: async ({ page }, use) => {
     const loginPage = new LoginPage(page);
+    await page.goto("/");
     await performLogin(loginPage, "standard_user", "secret_sauce");
+    await page.waitForURL("**/inventory.html");
     await use(page);
   },
 });
+
+export { expect };
