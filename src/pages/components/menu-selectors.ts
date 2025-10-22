acd4bdcae7bc08e3f601ed23e1139dc59496f2bf
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-22 15:31:06 +0800

    feat (menu-selectors): implement selector fields for menu component

---
+++ src/pages/components/menu-selectors.ts
@@ -0,0 +1,7 @@
+export const MENU_SELECTORS = {
+  MENU_CONTAINER: "menu-container",
+  MENU_LINK_ALL_ITEMS: "menu-link-all-items",
+  MENU_LINK_ABOUT: "menu-link-about",
+  MENU_LINK_LOGOUT: "menu-link-logout",
+  MENU_LINK_RESET_STATE: "menu-link-reset-state",
+} as const;
