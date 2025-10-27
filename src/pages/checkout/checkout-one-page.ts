da01f1a31b6cf147ee62e4486939c1990c96165c
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-27 22:55:48 +0800

    feat (checkout-one-page): implement locators and actions for checkout one page

---
+++ src/pages/checkout/checkout-one-page.ts
@@ -0,0 +1,49 @@
+import { Locator, Page } from "@playwright/test";
+import { CHECKOUT_ONE_SELECTORS } from "./checkout-one-selectors";
+import { MenuComponent } from "@pages/components/menu-component";
+import { HeaderComponent } from "@pages/components/header-component";
+
+export class CheckoutOnePage {
+  private _page: Page;
+  private _locators: Record<keyof typeof CHECKOUT_ONE_SELECTORS, Locator>;
+  public menu: MenuComponent;
+  public header: HeaderComponent;
+
+  constructor(page: Page) {
+    this._page = page;
+    this._locators = {
+      CHECKOUT_ONE_FNAME: this._page.getByPlaceholder("First Name"),
+      CHECKOUT_ONE_LNAME: this._page.getByPlaceholder("Last Name"),
+      CHECKOUT_ONE_ZIP: this._page.getByPlaceholder("Zip/Postal Code"),
+      CHECKOUT_ONE_CANCEL_BUTTON: this._page.getByRole("button", {
+        name: "Go back Cancel",
+      }),
+      CHECKOUT_ONE_CONTINUE_BUTTON: this._page.getByRole("button", {
+        name: "Continue",
+      }),
+    };
+    this.menu = new MenuComponent(page);
+    this.header = new HeaderComponent(page);
+  }
+
+  async enterText(
+    field: keyof typeof CHECKOUT_ONE_SELECTORS,
+    text: string
+  ): Promise<void> {
+    await this._locators[field].fill(text);
+  }
+
+  async click(field: keyof typeof CHECKOUT_ONE_SELECTORS): Promise<void> {
+    await this._locators[field].click();
+  }
+
+  async getText(field: keyof typeof CHECKOUT_ONE_SELECTORS): Promise<string> {
+    return await this._locators[field].innerText();
+  }
+
+  async getLocator(
+    field: keyof typeof CHECKOUT_ONE_SELECTORS
+  ): Promise<Locator> {
+    return await this._locators[field];
+  }
+}
