cc709f24a53e75ff6ff3b9e575ac05b0e84fc87f
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-27 23:25:37 +0800

    feat (cart-fixture): implement 'cartReady' fixture for checkout one page

---
+++ src/fixtures/cart-fixture.ts
@@ -0,0 +1,15 @@
+import { test as base } from "@fixtures/inventory-fixture";
+import { CheckoutOnePage } from "@pages/checkout/checkout-one-page";
+
+// loggedIn -> inventoryReady -> cartReady (You are here!)
+export const test = base.extend<{ cartReady: CheckoutOnePage }>({
+  cartReady: async ({ inventoryReady }, use) => {
+    // Performs click action
+    await inventoryReady.click("CART_CHECKOUT_BUTTON");
+    await inventoryReady.page.waitForURL("**/checkout-step-one.html");
+
+    // Returns the checkout step one page object
+    const checkoutOne = new CheckoutOnePage(inventoryReady.page);
+    await use(checkoutOne);
+  },
+});
