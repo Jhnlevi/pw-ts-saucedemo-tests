44b08cb6487db9b17d1ab7a38d3a9181b8b72a36
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-28 20:29:58 +0800

    refactor: add comments for clarity and modify await use() for each of the current fixture classes

--- src/fixtures/inventory-fixture.ts
+++ src/fixtures/inventory-fixture.ts
@@ -1,5 +1,4 @@
 import { test as base } from "./logged-in.fixture";
-import { InventoryPage } from "@pages/inventory/inventory-page";
 import { addItems } from "@helpers/inventory-helper";
 import { InventoryItem, inventoryItems } from "@data/inventory-item-data";
 import { CartPage } from "@pages/cart/cart-page";
@@ -9,20 +8,16 @@ const items: InventoryItem[] = [...inventoryItems];
 // loggedIn -> inventoryReady (You are here!)
 export const test = base.extend<{ inventoryReady: CartPage }>({
   inventoryReady: async ({ loggedIn }, use) => {
-    // Initializes the inventory page because the previous fixture (loggedIn) returns page.
-    const inventory = new InventoryPage(loggedIn);
+    // Add items to the cart and takes the user to the cart page
+    await addItems(loggedIn, items);
 
-    // Add items to the cart
-    await addItems(inventory, items);
-
-    // Takes the user to the cart page
-    await inventory.header.click("HEADER_CART_BUTTON");
-    await loggedIn.waitForURL("**/cart.html");
+    await loggedIn.header.click("HEADER_CART_BUTTON");
+    await loggedIn.page.waitForURL("**/cart.html");
 
     // Returns the cart page object
-    const cartPage = new CartPage(loggedIn);
-    await use(cartPage);
+    await use(new CartPage(loggedIn.page));
   },
 });
 
+// Exports expect from base fixture (logged-in fuxture)
 export { expect } from "@fixtures/logged-in.fixture";
